700e64283fe32905e3f942bb130735e7e27c0db3
github username <me@joshuahill.dev>
2020-01-02 13:37:32 -0600

    first commit in 2020

--- notesApp/app.js
+++ notesApp/app.js
@@ -1,34 +1,42 @@
 const chalk = require("chalk");
 const yargs = require("yargs");
 
+const getNotes = require("./notes.js");
+
+yargs.version("1.2.2");
+
+// making some commands: add, remove, read, and list
+
 yargs.command({
   command: "add",
   describe: "add a note",
-  handler: () => {
-    console.log("adding that note");
+  handler: function() {
+    console.log("note added");
   }
 });
 
 yargs.command({
   command: "remove",
-  describe: "delete a note",
-  handler: () => {
-    console.log(chalk.red("deleting note"));
+  describe: "remove the note",
+  handler: function() {
+    console.log("removed the note");
+  }
+});
+
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function() {
+    console.log("reading the note");
   }
 });
 
 yargs.command({
   command: "list",
   describe: "list all notes",
-  handler: () => {
-    console.log(chalk.blue("show me the notes"));
+  handler: function() {
+    console.log("listing all the notes");
   }
 });
 
-yargs.command({
-  command: "read",
-  describe: "read a specific note",
-  handler: () => {
-    console.log(chalk.blue("read your note bud"));
-  }
-});
+console.log(yargs.argv);
