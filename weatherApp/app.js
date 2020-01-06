59e9270e4f4934e6c12a46c50e208d06fb2d4f64
github username <me@joshuahill.dev>
2020-01-06 17:48:29 -0600

    finished section 33

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,15 +1,30 @@
 const request = require("request");
 
-const key = require("./key.js");
-const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233`;
+const keys = require("./key.js");
+cityLat = 0.0;
+cityLng = 0.0;
+// const weatherURL = `https://api.darksky.net/forecast/${keys.weather}/${cityLat},${cityLng}`;
+const mapsURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Sacramento.json?access_token=${keys.maps}&limit=1`;
 
-request({ url: url, json: true }, (error, response) => {
-  console.log(
-    response.body.daily.data[0].summary +
-      " The temp is: " +
-      response.body.currently.temperature +
-      " degrees, with a " +
-      response.body.currently.precipProbability +
-      "% chance of rain."
-  );
+request({ url: mapsURL, json: true }, (error, response) => {
+  console.log(response.body.features[0].geometry.coordinates);
+  cityLat = response.body.features[0].geometry.coordinates[0];
+  cityLng = response.body.features[0].geometry.coordinates[1];
 });
+
+request(
+  {
+    url: `https://api.darksky.net/forecast/${keys.weather}/${cityLat},${cityLng}`,
+    json: true
+  },
+  (error, response) => {
+    console.log(
+      response.body.daily.data[0].summary +
+        " The temp is: " +
+        response.body.currently.temperature +
+        " degrees, with a " +
+        response.body.currently.precipProbability +
+        "% chance of rain."
+    );
+  }
+);
