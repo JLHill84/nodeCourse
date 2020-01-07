1f212d406a2844a34a24b3e018467705f637fce4
github username <me@joshuahill.dev>
2020-01-06 18:10:07 -0600

    34 finished

--- weatherApp/app.js
+++ weatherApp/app.js
@@ -1,30 +1,46 @@
 const request = require("request");
 
 const keys = require("./key.js");
-cityLat = 0.0;
-cityLng = 0.0;
 // const weatherURL = `https://api.darksky.net/forecast/${keys.weather}/${cityLat},${cityLng}`;
-const mapsURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/Sacramento.json?access_token=${keys.maps}&limit=1`;
+const mapsURL = `https://api.mapbox.com/geocoding/v5/mapbox.places/houston.json?access_token=${keys.maps}&limit=1`;
+
+let cityLat;
+let cityLng;
+let cityName;
 
 request({ url: mapsURL, json: true }, (error, response) => {
-  console.log(response.body.features[0].geometry.coordinates);
-  cityLat = response.body.features[0].geometry.coordinates[1];
-  cityLng = response.body.features[0].geometry.coordinates[0];
-});
+  if (error) {
+    console.log("there was a problem contacting the geolocation API");
+  } else if (response.body.error || response.body.features.length === 0) {
+    console.log("bad input, couldn't find that location");
+    return;
+  } else {
+    console.log(response.body.features[0].geometry.coordinates);
+    cityLat = response.body.features[0].geometry.coordinates[1];
+    cityLng = response.body.features[0].geometry.coordinates[0];
+    cityName = response.body.features[0].place_name;
 
-request(
-  {
-    url: `https://api.darksky.net/forecast/${keys.weather}/${cityLat},${cityLng}`,
-    json: true
-  },
-  (error, response) => {
-    console.log(
-      response.body.daily.data[0].summary +
-        " The temp is: " +
-        response.body.currently.temperature +
-        " degrees, with a " +
-        response.body.currently.precipProbability +
-        "% chance of rain."
+    request(
+      {
+        url: `https://api.darksky.net/forecast/${keys.weather}/${cityLat},${cityLng}`,
+        json: true
+      },
+      (error, response) => {
+        if (error) {
+          console.log("error communicating with DarkSky API");
+        } else {
+          console.log(
+            cityName +
+              ". " +
+              response.body.daily.data[0].summary +
+              " The temp is: " +
+              response.body.currently.temperature +
+              " degrees, with a " +
+              response.body.currently.precipProbability +
+              "% chance of rain."
+          );
+        }
+      }
     );
   }
-);
+});
