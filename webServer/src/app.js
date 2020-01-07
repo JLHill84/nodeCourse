60fed20ec3dd518bb1612623f9ae7e2afa7dc112
github username <me@joshuahill.dev>
2020-01-07 17:58:40 -0600

    lesson 47

--- webServer/src/app.js
+++ webServer/src/app.js
@@ -3,7 +3,29 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather Homepage",
+    name: "Joshua Hill"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About page",
+    name: "Joshua Hill"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    message: "Let's see if this is working as intended."
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send([{ forecast: "weather page", location: "all the weather" }]);
